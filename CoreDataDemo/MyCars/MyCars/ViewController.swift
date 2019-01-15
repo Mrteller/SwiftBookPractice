076e01383e090257e167f9841db464db691cf6fe
MrTeller <pav.tolmach@gmail.com>
2019-01-15 20:50:26 +0300

    Prerequisites for second project. End of lection 8.

--- CoreDataDemo/MyCars/MyCars/ViewController.swift
+++ CoreDataDemo/MyCars/MyCars/ViewController.swift
@@ -9,12 +9,30 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    //Paul's notes: Much better then before. We have controller var. But we could do even better and make it static
+    lazy var context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    @IBOutlet weak var markLabel: UILabel!
+    @IBOutlet weak var modelLabel: UILabel!
+    @IBOutlet weak var carImageView: UIImageView!
+    @IBOutlet weak var lastTimeStartedLabel: UILabel!
+    @IBOutlet weak var numberOfTripsLabel: UILabel!
+    @IBOutlet weak var ratingsLabel: UILabel!
+    @IBOutlet weak var myChoiceImageView: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func segmentedCtrlPressed(_ sender: UISegmentedControl) {
+    }
+    
+    @IBAction func startEnginePressed(_ sender: UIButton) {
+    }
+    @IBAction func rateItPressed(_ sender: UIButton) {
+    }
 }
 
