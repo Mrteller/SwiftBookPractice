2f75bc7af719ee5b33589639db55dcec1fe5cf50
MrTeller <pav.tolmach@gmail.com>
2019-01-15 20:07:36 +0300

    Building UI

---
+++ CoreDataDemo/MyCars/MyCars/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyCars
+//
+//  Created by Paul on 15.01.2019.
+//  Copyright © 2019 Paul. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
