1533cbebb24b83a25a16d3aaf76f85eed603c98e
MrTeller <pav.tolmach@gmail.com>
2019-01-22 20:55:24 +0300

    End of lection 16

--- Meal time/Meal time/ViewController.swift
+++ Meal time/Meal time/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController, UITableViewDataSource {
     var context: NSManagedObjectContext!
     @IBOutlet weak var tableView: UITableView!
     var array = [Date]()
+    var person: Person!
     
     // Paul's note: well done! Init with closure, correct lazy  usage
     lazy var dateFormatter: DateFormatter  = {
@@ -26,19 +27,39 @@ class ViewController: UIViewController, UITableViewDataSource {
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+        
+        let personName = "Max"
+        let fetchRequest : NSFetchRequest<Person> = Person.fetchRequest()
+        fetchRequest.predicate = NSPredicate(format: "name = %@", personName)
+        do {
+            let results = try context.fetch(fetchRequest)
+            if results.isEmpty {
+                person = Person(context: context)
+                person.name = personName
+            } else {
+                person = results.first
+            }
+        } catch let error as NSError {
+            print(error.userInfo)
+        }
     }
     //MARK: UITableViewDataSource delegate methods (delegate linked via Storyboard)
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return array.count
+        guard let meals = person.meals else { return 1 }
+        return meals.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         if let cell = tableView.dequeueReusableCell(withIdentifier: "Cell") {
-            let date = array[indexPath.row]
-            cell.textLabel?.text = dateFormatter.string(from: date)
+            
+            guard let meal = person.meals?[indexPath.row] as? Meal, let mealDate = meal.date else
+            {
+                return cell
+            }
+            cell.textLabel?.text = dateFormatter.string(from: mealDate as Date) // Paul's note:NSDate is bridged to Date so we can avoid optionality if cast it here, not before
             // Paul's note: could just go without dateFormatter and get a LOCALIZED one
-            cell.textLabel?.text = DateFormatter.localizedString(from: date, dateStyle: .short, timeStyle: .short)
+            // cell.textLabel?.text = DateFormatter.localizedString(from: date, dateStyle: .short, timeStyle: .short)
             return cell
         } else {
             return UITableViewCell(style: .default, reuseIdentifier: "Cell")
@@ -49,5 +70,28 @@ class ViewController: UIViewController, UITableViewDataSource {
     /*func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
         return "My happy meal time"
     } */
+    
+    @IBAction func addButtonPressed(_ sender: Any) {
+        let meal = Meal(context: context)
+        meal.date = NSDate()
+        
+        let meals = person.meals?.mutableCopy() as? NSMutableOrderedSet
+        meals?.add(meal)
+        person.meals = meals
+        
+        // Paul's note: we could just do it much more efficient and natural
+        // person.addToMeals(meal) // or even more flexible if we wanted sorting or adding at the beginning
+        // person.insertIntoMeals(meal, at: 0)
+        // Paul's note: We have a func saveContext - why do we repeat ouselves
+        do {
+            try context.save()
+        } catch let error as NSError {
+            print("Error: \(error), userInfo \(error.userInfo)")
+        }
+        
+        
+        tableView.reloadData()
+    }
+    
 }
 
